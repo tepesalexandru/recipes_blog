bdcb93eeed2db5cd481ade2fd595ff1ebaa9e7a7
birtapaulamaria31 <paulamariabirta1002@gmail.com>
2022-05-14 15:46:12 +0300

    added boostrap

--- index.php
+++ index.php
@@ -6,22 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Family Recipes</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <?php include 'seed_database.php'; ?>
     <?php
-    $sql = "SELECT * FROM Users";
+    $sql = "SELECT Articles.id as id, Articles.title as title, Users.firstName as author
+    FROM Articles 
+    INNER JOIN Users 
+    ON Articles.authorId = Users.Id";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         // output data of each row
         while ($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"] . " - Name: " . $row["firstName"] . " " . $row["lastName"] . "<br>";
+            echo "id: " . $row["id"] . " - Title: " . $row["title"] . ", author: " . $row["author"] . "<br>";
         }
     } else {
         echo "0 results";
     }
     ?>
+    <button type="button" class="btn btn-primary">Save</button>
+    <button type="button" class="btn btn-danger">Danger</button>
 </body>
 
 </html>
