bc19078b6904f175bed57a89594b76ebc3ed12f4
Tepes Alexandru <tepes_alexandru@outlook.com>
2022-05-28 13:18:50 +0300

    manage articles as admin

--- admin_panel.php
+++ admin_panel.php
@@ -1,1 +1,36 @@
-<div>Admin panel</div>
+<?php
+require 'header.php';
+require './includes/dbh.inc.php';
+require './includes/articles.inc.php';
+
+$articles = getBlogs($conn);
+
+?>
+
+<table class="table" style="vertical-align:middle">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Author</th>
+            <th scope="col">Title</th>
+            <th scope="col">Published On</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $index = 1;
+        foreach ($articles as $article) {
+            echo $twig->render("article_row.html.twig", [
+                'id' => $article['id'],
+                'index' => $index,
+                'title' => $article['title'],
+                'publishedOn' => $article['publishedOn'],
+                'author' => $article['author'],
+            ]);
+            $index++;
+        }
+        ?>
+
+    </tbody>
+</table>
--- includes/articles.inc.php
+++ includes/articles.inc.php
@@ -2,7 +2,7 @@
 require 'dbh.inc.php';
 function getBlogs($conn)
 {
-    $sql = "SELECT Articles.Id as id, title, content, username as author
+    $sql = "SELECT Articles.Id as id, title, content, username as author, publishedOn
     FROM Articles
     JOIN Users ON Articles.AuthorId = Users.Id";
     return $conn->query($sql);
