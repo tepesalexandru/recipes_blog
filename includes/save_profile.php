c2b6328125a86347af60eb7e5c9fc53356ba0a64
Tepes Alexandru <tepes_alexandru@outlook.com>
2022-05-30 22:51:11 +0300

    only update your profile

--- includes/save_profile.php
+++ includes/save_profile.php
@@ -5,12 +5,13 @@ if (isset($_POST['save-profile-submit'])) {
     $data = file_get_contents($_FILES['profile-picture']['tmp_name']);
     $data = mysqli_real_escape_string($conn, $data);
     $username = $_POST['username'];
+    $userId = $_SESSION['userId'];
 
     if (empty($username)) {
         header("Location: ../profile.php?error=emptyfields");
         exit();
     } else {
-        $sql = "UPDATE Users SET Username=?, imageBlob='$data'";
+        $sql = "UPDATE Users SET Username=?, imageBlob='$data' WHERE Users.Id = $userId";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: ../profile.php?error=sqlerror");
