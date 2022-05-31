c367ad7756813c018fd2d48e2048a1714cf0e12e
Tepes Alexandru <tepes_alexandru@outlook.com>
2022-06-01 00:51:19 +0300

    style updates

--- view_articles.php
+++ view_articles.php
@@ -22,4 +22,20 @@ $articles = getBlogs($conn);
         }
         ?>
     </div>
+    <div class="row">
+        <?php
+        foreach ($articles as $card) {
+            echo $twig->render(
+                'blog_card.html.twig',
+                [
+                    'title' => $card['title'], 
+                    'content' => substr($card['content'], 0, 200), 
+                    'author' => $card['author'], 
+                    'id' => $card['id'],
+                    'imageSrc' => "data:image/jpeg;base64," . base64_encode($card['imageBlob'])
+                ]
+            );
+        }
+        ?>
+    </div>
 </div>
